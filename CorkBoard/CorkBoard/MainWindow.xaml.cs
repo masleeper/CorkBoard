9f73dc3f6885a02fd1afc5b23940d12681f20274
Vincent Ma <vincent.ma810@gmail.com>
2019-09-25 13:14:51 -0400

    Integrate basic timer code

--- CorkBoard/CorkBoard/MainWindow.xaml.cs
+++ CorkBoard/CorkBoard/MainWindow.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -25,10 +27,22 @@ namespace CorkBoard
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.Timer timer;
+        private int clktimer;
+        private int wxtimer;
+        private int imgtimer;
+        private string lastWrittenTime;
+        private Settings settings;
+
         public MainWindow()
         {
             InitializeComponent();
-           
+            settings = new Settings();
+
+            clktimer = 0;
+            wxtimer = 0;
+            imgtimer = 0;
+            lastWrittenTime = DateTime.Now.ToString("hh:mm tt");
 
             Weather weather = new Weather();
             Weather.WeatherInfo weatherInfo = weather.getWeather("https://api.weather.gov/stations/KLAF/observations?limit=1");
@@ -47,6 +61,59 @@ namespace CorkBoard
             DayBlock.Text = DateTime.Now.DayOfWeek.ToString();
             DateBlock.Text = DateTime.Now.ToString("MMMM dd,yyyy", CultureInfo.InvariantCulture);
             Show();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += new System.EventHandler(timerTick);
+            timer.Start();
+        }
+
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            clktimer = ++clktimer % settings.getClockRefresh();
+            if (clktimer == 0)
+            {
+                string currentTime = DateTime.Now.ToString("h:mm tt");
+                if (lastWrittenTime.CompareTo(currentTime) != 0)
+                {
+                    lastWrittenTime = currentTime;
+                    updateTime(lastWrittenTime);
+                    Console.WriteLine("Update clock to " + lastWrittenTime);
+                }
+            }
+
+            wxtimer = ++wxtimer % settings.getWeatherRefresh();
+            if (wxtimer == 0)
+            {
+                Console.WriteLine("Update weather.");
+            }
+
+            imgtimer = ++imgtimer % settings.getImgRefresh();
+            if (imgtimer == 0)
+            {
+                Console.WriteLine("Update image.");
+            }
+
+        }
+        public void updateTime(string time)
+        {
+            TimeBlock.Text = time;
+        }
+
+        public void updateDay(string day)
+        {
+            DayBlock.Text = day;
+        }
+
+        public void updateDate(string date)
+        {
+            DateBlock.Text = date;
+        }
+
+        public void updateTemp(string temp)
+        {
+            TempBlock.Text = temp;
         }
     }
 }
