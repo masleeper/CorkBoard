c05dcbeec6a4f490473c43daf61f9dbe3b2b2a56
Thomas <43069746+nibarder@users.noreply.github.com>
2019-10-13 19:52:42 -0400

    Network Update
    NOTE: This update includes changes to core method parameters. 
           Affected files:
                       MainWindow.cs
                       Forecast.cs
                       Weather.cs
    
    Added getAlerts(string state, string county) method in Alerts class
    modified getWeather(string URL) to => getWeather(string zone) where zone is a weather station ID (ex. "KLAF" for west lafayette)
    modified getForecast(string URL) to => getForecast(string zone) where zone is a forecast region ID (ex. "IND" is southern indiana)
    changed MainWindow call to getForecast(...) and getWeather(...) to updated parameter input

---
+++ CorkBoard/CorkBoard/CorkBoard/Network/Alerts.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorkBoard.Core;
+
+namespace CorkBoard.Network
+{
+    public class Alerts
+    {
+        public struct AlertData
+        {
+            public string severity;
+            public string msg;
+        }
+
+        public Alerts()
+        {
+
+        }
+
+        public List<AlertData> getAlerts(string state, string county)
+        {
+            List<AlertData> data = new List<AlertData>();
+            string call = "https://api.weather.gov/alerts/active/area/" + state;
+            if (county == null || county == "")
+            {
+                AlertData err;
+                err.severity = "ERROR";
+                err.msg = "Empty county value";
+                data.Add(err);
+                return data;
+            }
+            if (state == null || state == "")
+            {
+                AlertData err;
+                err.severity = "NO API CALL";
+                err.msg = "Empty state value";
+                data.Add(err);
+                return data;
+            }
+            string webText = "";
+            dynamic wJson = null;
+            try
+            {
+                webText = new Net().getWebText(call);
+                wJson = HelperFunctions.convertToDynamic(webText);
+            }
+            catch (Exception e)
+            {
+                AlertData err;
+                err.severity = "ERROR";
+                err.msg = "Invalid API Call";
+                data.Add(err);
+            }
+
+            try
+            {
+                
+                if (wJson != null &&
+                    wJson["features"] != null &&
+                    wJson["features"][0] != null)
+                {
+                    int i = 0;
+                    while (wJson["features"][i] != null)
+                    {
+                        AlertData data1;
+                        if (((string)wJson["features"][i]["properties"]["areaDesc"]).Contains(county))
+                        {
+                            data1.msg = wJson["features"][i]["properties"]["event"] != null ? (string)wJson["features"][i]["properties"]["event"] : "";
+                            data1.severity = wJson["features"][i]["properties"]["severity"] != null ? (string)wJson["features"][i]["properties"]["severity"] : "";
+
+                            data.Add(data1);
+                        }
+
+                        i++;
+                    }
+                }
+                else
+                {
+                    AlertData err;
+                    err.severity = "NONE";
+                    err.msg = "No alerts found value";
+                    data.Add(err);
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+            if(data.Count == 0)
+            {
+                AlertData noAlert;
+                noAlert.msg = "No ALerts";
+                noAlert.severity = "NONE";
+                data.Add(noAlert);
+            }
+            return data;
+        }
+
+    }
+}
---
+++ CorkBoard/CorkBoard/CorkBoard/Network/Net.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CorkBoard.Network
+{
+    class Net
+    {
+        public string getWebText(string url)
+        {
+
+            WebClient wc = new WebClient();
+            HttpRequestHeader requestHeader;
+            wc.Headers.Add("user-agent", "CorkBoard");
+            try
+            {
+                string text = wc.DownloadString((url.Contains("://") || url.Contains(":\\"))? url : "http://" + url);
+                
+                return text;
+            }catch (Exception)
+            {
+                return "error";
+            }
+
+            
+        }
+        public BitmapImage getWebImage(string url)
+        {
+
+            try
+            {
+                BitmapImage bmi = new BitmapImage();
+                bmi.BeginInit();
+                bmi.UriSource = new System.Uri(url);
+                bmi.EndInit();
+                return bmi;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+
+        public string getWebFile(string url)
+        {
+
+            WebClient wc = new WebClient();
+            HttpRequestHeader requestHeader;
+            wc.Headers.Add("user-agent", "CorkBoard");
+            string fileType = url.Split('.')[url.Split('.').Length - 1];
+            try
+            {
+                string rnd = ".\\FILE_" + new Random().Next(111111, 99999999)+"." + fileType;
+                wc.DownloadFile((url.Contains("://") || url.Contains(":\\")) ? url : "http://" + url, rnd);
+
+                return rnd;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+        }
+    }
+}
